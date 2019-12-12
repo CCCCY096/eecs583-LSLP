7d128b6dd45d9ca2c94e2ec246be48b7020b5279
Cangsu Lei <lcsu@umich.edu>
2019-12-12 14:50:59 -0500

    testcases and pngs

--- benchmarks/test1.c
+++ benchmarks/test1.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-const long long n = 100000000;
+const long long n = 5000;
 long A[n];
 long B[n];
 long C[n];
 
 // test1: SLP can vectorize
 int main() {
-    for (long long i = 0; i < n-2; i=i+2) {
-        A[i+0] = B[i+0] + (C[i+0]+3);
-        A[i+1] = (C[i+1]-4) + B[i+1];
+    for (int j = 0; j < 1000000; ++j) {
+        for (long long i = 0; i < n-2; i=i+2) {
+          A[i+0] = B[i+0] + (C[i+0]+3);
+          A[i+1] = (C[i+1]-4) + B[i+1];
+        }
     }
 
     printf("%ld %ld %ld\n", A[1000], B[1000], C[1000]);
--- benchmarks/test4.c
+++ benchmarks/test4.c
@@ -2,18 +2,45 @@
 // test4: SLP fails. Associativity Mismatch and Multi-Node
 #include <stdio.h>
 
-const int n = 50000000;
+const int n = 5000;
 long A[n] = {0};
 long B[n] = {1};
 long C[n] = {1};
 long D[n] = {1};
 long E[n] = {1};
 
-int main() {
-    for (long i = 0; i < n-2; i= i+2) {
-        A[i+0] = A[i+0] & (B[i+0] + C[i+0]) & (D[i+0] + E[i+0]);
-        A[i+1] = (D[i+1] + E[i+1]) & (B[i+1] + C[i+1]) & A[i+1];
+void init(long *a) {
+    for (int i = 0; i < n ; ++i) {
+        a[i] = i;
     }
-    printf("%ld %ld %ld\n", A[1000], B[1000], C[1000]);
+}
+
+long verify(long *a) {
+    int total = 0;
+    for (int i = 0; i < n; ++i) {
+      total += a[i];
+    }
+    return total;
+}
+
+int main() {
+    init(A);
+    init(B);
+    init(C);
+    init(D);
+    init(E);
+    for (int j = 0; j < 100000; ++j) {
+        for (long i = 0; i < n-2; i= i+2) {
+          A[i+0] = A[i+0] & (B[i+0] + C[i+0]) & (D[i+0] + E[i+0]);
+          A[i+1] = (D[i+1] + E[i+1]) & (B[i+1] + C[i+1]) & A[i+1];
+        }
+    }
+
+    printf("%ld \n", verify(A));
+    printf("%ld \n", verify(B));
+    printf("%ld \n", verify(C));
+    printf("%ld \n", verify(D));
+    printf("%ld \n", verify(E));
+
 
 }
